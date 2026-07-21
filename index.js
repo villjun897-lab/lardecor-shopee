5e27e3919f89a8be4002a8bf05fe4b5fa195510d
Junio <startdigital160@gmail.com>
2026-07-21 01:12:46 -0300

    fix ssl postgres

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ const INTERVALO_MINUTOS = 12;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const NICHOS = [
